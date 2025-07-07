e12dc63a53aff2d6d8b44f9225bb21d9ef102175
UnTamed Fury <168332215+UnTamed-Fury@users.noreply.github.com>
2025-07-07 15:54:10 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 3000,       // Change to any port you prefer
-    host: '0.0.0.0'   // Allow access from other devices (LAN or remote)
+    port: 3000,
   }
 })
